26872cf15677a0bfbff6a59514f018baaf65769f
jbarry89 <joelbarry8907@hotmail.com>
2025-03-06 14:38:37 -0500

    added some code for port to listen to graphql

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,23 @@
 import express from 'express';
+import { ApolloServer } from '@apollo/server';
+import { expressMiddleware } from '@apollo/server/express4';
 import path from 'node:path';
+import dotenv from 'dotenv';
 import db from './config/connection.js';
-import routes from './routes/index.js';
+import { typeDefs } from './graphql/typeDefs';
+import { resolvers } from './graphql/resolvers';
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+// Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -14,8 +26,15 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+const startServer = async () => {
+  await server.start();
+  app.use('/graphql', expressMiddleware(server));
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+  db.once('open', () => {
+    app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}/graphql`));
+  });
+  
+};
+
+startServer();
+
