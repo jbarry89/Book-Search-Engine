fe803179db7f9dbc42c524a826219d25fc621cad
jbarry89 <joelbarry8907@hotmail.com>
2025-03-06 14:40:19 -0500

    Added folder schema for typeDefs and resolver files

---
+++ server/src/schemas/typeDefs.ts
@@ -0,0 +1,33 @@
+export const typeDefs =`
+  type User {
+    _id: ID!
+    username: String!
+    email: String!
+    savedBooks: [Book]
+  }
+
+  type Book {
+    bookId: ID!
+    authors: [String]
+    title: String!
+    description: String
+    image: String
+    link: String
+  }
+
+  type Auth {
+    token: String!
+    user: User
+  }
+
+  type Query {
+    me: User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(bookId: ID!, title: String!, authors: [String], description: String, image: String, link: String): User
+    removeBook(bookId: ID!): User
+  }
+`;
