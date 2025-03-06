d9b0654257d448fcbf9a7182642e647e64894221
jbarry89 <joelbarry8907@hotmail.com>
2025-03-06 15:32:41 -0500

    Modified code to connect to the Graphql server

--- server/src/services/auth.ts
+++ server/src/services/auth.ts
@@ -1,4 +1,4 @@
-import type { Request, Response, NextFunction } from 'express';
+// import type { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 
 import dotenv from 'dotenv';
@@ -10,24 +10,23 @@ interface JwtPayload {
   email: string,
 }
 
-export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
-  const authHeader = req.headers.authorization;
+export const authMiddleware = ({ req }: { req: Request }) => {
+  const authHeader = req.headers.authorization || "";
+  let user = null;
 
-  if (authHeader) {
-    const token = authHeader.split(' ')[1];
+  // Check if token exist
+  if (authHeader.starstWith("Bearer")) {
+    const token = authHeader.split(" ")[1];  // Get (extract) the token part after 'Bearer'
 
-    const secretKey = process.env.JWT_SECRET_KEY || '';
+    try {
+      // Decode the token to get user data
+      const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY || "") as JwtPayload;
+      user = decoded; // Attach the decoded user to context
+    } catch (error) {
+      console.error("Invalid token or expired token");
+    }
 
-    jwt.verify(token, secretKey, (err, user) => {
-      if (err) {
-        return res.sendStatus(403); // Forbidden
-      }
-
-      req.user = user as JwtPayload;
-      return next();
-    });
-  } else {
-    res.sendStatus(401); // Unauthorized
+    return {user}; // return user data or null (if token is invalid)
   }
 };
 
@@ -35,5 +34,5 @@ export const signToken = (username: string, email: string, _id: unknown) => {
   const payload = { username, email, _id };
   const secretKey = process.env.JWT_SECRET_KEY || '';
 
-  return jwt.sign(payload, secretKey, { expiresIn: '1h' });
+  return jwt.sign(payload, secretKey, { expiresIn: '1h' });  // token expires in 1 hour
 };
