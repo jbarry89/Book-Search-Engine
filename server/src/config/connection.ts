c4764e139a530c7c6efa63b509dbb808d55438fa
jbarry89 <joelbarry8907@hotmail.com>
2025-03-06 16:14:41 -0500

    added code to catch errors when trying to connect to Database

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -1,5 +1,18 @@
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
+dotenv.config();
+
+const connectDB = async () =>{
+  try {
+    await mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
+    console.log('MongoDB connected successfully!');
+} catch (error) {
+    console.error('MongoDB connection error:', error);
+    process.exit(1); // Exit the process
+  }
+
+};
+connectDB();
 
 export default mongoose.connection;
