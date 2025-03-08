93eb84f6f92bc79a17ed52fe1ee075a1ca1e0a2f
jbarry89 <joelbarry8907@hotmail.com>
2025-03-08 03:38:52 -0500

    Added Apollo Provider code to make the request work with Apollo server.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,21 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
+import {ApolloProvider, ApolloClient, InMemoryCache} from '@apollo/client';
 
 import Navbar from './components/Navbar';
 
+// Create Apollo Client instance
+const client = new ApolloClient({
+  uri: '/graphql', // Ensure this matches your backend GraphQL endpoint
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
-    <>
+    <ApolloProvider client={client}>
       <Navbar />
       <Outlet />
-    </>
+    </ApolloProvider>
   );
 }
 
