380675e1b71f59ab30fdd2c8c25b33562509282d
jbarry89 <joelbarry8907@hotmail.com>
2025-03-13 23:05:36 -0400

    Added a build script in 'vite.config.ts' file.

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Ensure the output is directed to the 'dist' folder
+  },
   server: {
     port: 3000,
     open: true,
