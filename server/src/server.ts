a9459a5f78617b316090ebd8ff2028d124339dc8
jbarry89 <joelbarry8907@hotmail.com>
2025-03-13 21:59:55 -0400

    Added fileURLPath to fix rendor deploying issue

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,16 @@
 import express from 'express';
 import path from 'node:path';
 import type { Request, Response } from 'express';
+import { fileURLToPath } from 'url';
 import db from './config/connection.js'
 import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@apollo/server/express4';
 import { typeDefs, resolvers } from './schemas/index.js';
 import { authenticateToken } from './services/auth.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 
 // Apollo Server Setup
 const server = new ApolloServer({
@@ -39,7 +44,7 @@ const startApolloServer = async () => {
     });
   }
 
-  app.listen(PORT, () => {
+  app.listen(PORT,  () => {
     console.log(`API server running on port ${PORT}!`);
     console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
   });
